7f8b263cb217031b654c9faf9f8802adcba18644
Sensanaty <salevic@lu-ka.me>
2022-06-25 12:54:25 +0200

    Add more helpful path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,10 @@ export default defineConfig({
   resolve: {
     alias: {
       "~/": `${path.resolve(__dirname, "src")}/`,
-      "#components/": `${path.resolve(__dirname, "src")}/components/`
+      "#components/": `${path.resolve(__dirname, "src")}/components/`,
+      "#assets/": `${path.resolve(__dirname, "src")}/assets/`,
+      "#composables/": `${path.resolve(__dirname, "src")}/composables/`,
+      "#store/": `${path.resolve(__dirname, "src")}/store/`,
     },
   },
 
