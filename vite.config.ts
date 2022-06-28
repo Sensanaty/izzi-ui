7e151b81634044a5f165de408a4dd6dd87016b69
Sensanaty <salevic@lu-ka.me>
2022-06-28 21:38:18 +0200

    Push up additional route aliases

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       "#assets/": `${path.resolve(__dirname, "src")}/assets/`,
       "#composables/": `${path.resolve(__dirname, "src")}/composables/`,
       "#store/": `${path.resolve(__dirname, "src")}/store/`,
+      "#types": `${path.resolve(__dirname, "")}/types/index.ts`,
     },
   },
 
