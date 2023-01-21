e5dfea613e1332c254f0550d84d2fd170dae4936
Sensanaty <salevic@lu-ka.me>
2023-01-21 20:51:29 +0100

    Implement an animated timer div

--- src/components/notifications/PopUp.vue
+++ src/components/notifications/PopUp.vue
@@ -1,9 +1,14 @@
 <template>
   <div
-    class="min-w-[350px] text-sm rounded mt-3 px-3 py-4 bg-neutral-900 text-white z-50 cursor-pointer border-2 border-transparent border-solid hover:bg-neutral-800"
+    class="min-w-[350px] text-sm rounded mt-3 bg-neutral-900 text-white z-50 cursor-pointer border-4 pt-0 px-3 pb-3 border-transparent border-solid hover:bg-neutral-800"
     :class="type"
     @click.prevent="destroy"
   >
+    <div v-if="autoHide" class="timer relative mb-3 -ml-4">
+      <div class="absolute bg-green-500 w-[1px] min-h-[3px]" />
+      <Motion :animate="transformScale" :transition="animationDuration" :initial="true" class="middle absolute left-[1px] bg-green-500 w-[1px] min-h-[3px]" />
+    </div>
+
     <p>{{ message }}</p>
   </div>
 </template>
@@ -11,6 +16,7 @@
 <script lang="ts" setup>
   import Notification from "~/types/store/notification";
   import { onMounted } from "vue";
+  import { Motion } from "motion/vue";
 
   const props = defineProps<{
     index: number,
@@ -20,9 +26,14 @@
     duration: Notification["duration"]
   }>();
 
+  const transformScale = { transform: ["scaleX(300)","scaleX(0)"] };
+  const animationDuration = { duration: props.duration / 1000, easing: "linear" };
+
   const emit = defineEmits(["destroyNotification"]);
 
   onMounted(() => {
+    if (!props.autoHide) { return; }
+
     setTimeout(destroy, props.duration);
   });
 
@@ -32,8 +43,15 @@
 </script>
 
 <style scoped>
-  .warn { @apply border-amber-400 }
-  .info { @apply border-blue-400 }
-  .succ { @apply border-emerald-600 }
-  .dang { @apply border-orange-700 }
+  .middle { transform-origin: left; }
+
+  .warn { @apply border-b-amber-400 }
+  .info { @apply border-b-blue-400 }
+  .succ { @apply border-b-emerald-600 }
+  .dang { @apply border-b-orange-700 }
+
+  .warn .timer > div { @apply bg-amber-400 }
+  .info .timer > div { @apply bg-blue-400 }
+  .succ .timer > div { @apply bg-emerald-600 }
+  .dang .timer > div { @apply bg-orange-700 }
 </style>
