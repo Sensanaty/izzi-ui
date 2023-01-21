4c239653de6708808275ec7a698cac295c925172
Sensanaty <salevic@lu-ka.me>
2023-01-21 18:27:00 +0100

    Create Notification container & popup child components

---
+++ src/components/notifications/NotificationContainer.vue
@@ -0,0 +1,25 @@
+<template>
+  <Teleport to="body">
+    <div class="font-mono inline-flex column flex-wrap fixed bottom bottom-[20px] right-[15px] max-w-[350px]">
+      <PopUp
+        v-for="(notification, index) in notifications.notifications"
+        :key="notification.id"
+        :index="index"
+        :type="notification.type"
+        :message="notification.message"
+        :auto-hide="notification.autoHide"
+        :duration="notification.duration"
+        @destroy-notification="destroy"
+      />
+    </div>
+  </Teleport>
+</template>
+
+<script lang="ts" setup>
+  import PopUp from "~components/notifications/PopUp.vue";
+  import useNotificationStore from "~/store/notification.js";
+
+  const notifications = useNotificationStore();
+
+  const destroy = (index: number) => { notifications.destroyNotification(index); };
+</script>
---
+++ src/components/notifications/PopUp.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :class="type" @click.prevent="$emit('destroyNotification', index)">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import Notification from "~/types/store/notification";
+
+  defineProps<{
+    index: number,
+    type: Notification["type"],
+    message: Notification["message"],
+    autoHide: Notification["autoHide"],
+    duration: Notification["duration"]
+  }>();
+
+  defineEmits(["destroyNotification"]);
+</script>
+
+<style scoped>
+  div {
+    @apply min-w-[350px] text-sm rounded mt-3 px-3 py-4 bg-neutral-900 text-white z-50 cursor-pointer border-2 border-transparent border-solid hover:bg-neutral-800;
+  }
+
+  .warn { @apply border-amber-400 }
+  .info { @apply border-blue-400 }
+  .succ { @apply border-emerald-600 }
+  .dang { @apply border-orange-700 }
+</style>
