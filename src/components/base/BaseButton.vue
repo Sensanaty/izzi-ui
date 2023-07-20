4f2613b6255c372d834bb04418764b236f8975b5
Luka <salevic@lu-ka.me>
2023-07-20 19:07:23 +0200

    Change styling of the BaseButton

--- src/components/base/BaseButton.vue
+++ src/components/base/BaseButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="flex items-center justify-center text-xl leading-tight py-1.5 px-4 rounded uppercase"
+    class="flex items-center justify-center leading-tight py-1.5 px-3 rounded uppercase"
     :class="[`${backgroundColor}`, `${backgroundHover}`, `${backgroundActive}`, hasDarkText ? 'text-black' : 'text-white']"
   >
     <slot />
@@ -13,19 +13,19 @@
       validator(value: string): boolean {
         return /^bg-[a-z].+-[1-9]00$/.test(value);
       },
-      default: "bg-blue-500"
+      default: "bg-sky-500"
     },
     backgroundHover: {
       validator(value: string): boolean {
         return /^hover:bg-[a-z].+-[1-9]00$/.test(value);
       },
-      default: "hover:bg-blue-700"
+      default: "hover:bg-cyan-700"
     },
     backgroundActive: {
       validator(value: string): boolean {
         return /^active:bg-[a-z].+-[1-9]00$/.test(value);
       },
-      default: "active:bg-blue-800"
+      default: "active:bg-cyan-800"
     },
     hasDarkText: {
       type: Boolean,
