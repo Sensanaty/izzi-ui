5d68d614a51c0b1797d12bd9ba1fffaf9a889993
Luka <salevic@lu-ka.me>
2023-07-20 12:46:48 +0200

    Fix bad API URL for Prod

--- src/modules/fetch.ts
+++ src/modules/fetch.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:8000/" : "https://api.izzcup.net/";
+const baseUrl = process.env.NODE_ENV === "development" ? "http://localhost:8000/" : "https://api.izzicup.net/";
 
 export const api = axios.create({
   baseURL: baseUrl,
