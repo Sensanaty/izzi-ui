fa0a379de688a6c1c57235286fca25cfe32e8e26
Luka <salevic@lu-ka.me>
2023-07-23 14:19:45 +0200

    Change table header sizings

--- src/components/admin/table/TableHeader.vue
+++ src/components/admin/table/TableHeader.vue
@@ -2,14 +2,14 @@
   <thead class="w-full bg-neutral-800">
     <tr class="w-full">
       <td class="w-7" />
-      <td class="w-[100px]">Part Number</td>
+      <td class="w-[150px]">Part Number</td>
       <td class="w-[90px]">Owner</td>
       <td class="!text-xs w-[85px]">Date Added</td>
       <td class="w-[200px]">Description</td>
       <td class="w-16">Available</td>
       <td class="w-16">Reserved</td>
       <td class="w-14">Sold</td>
-      <td class="w-15">Condition</td>
+      <td class="w-16">Condition</td>
       <td class="w-14">Cost 1</td>
       <td class="w-14">Sales 1</td>
       <td class="w-14">Order 1</td>
