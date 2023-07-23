4af2698096a547ffa292fca072585d463ebfac30
Luka <salevic@lu-ka.me>
2023-07-23 15:42:46 +0200

    Add a dedicated close button for the Base Modal

--- src/components/base/BaseModal.vue
+++ src/components/base/BaseModal.vue
@@ -3,10 +3,14 @@
     <Transition>
       <div
         v-if="isOpen"
-        class="fixed flex content-center justify-center z-[1] left-0 top-0 w-full h-full bg-neutral-900/80"
-        @click.prevent.once="emit('closeModal', false)"
+        class="fixed flex content-center justify-center left-0 top-0 w-full h-full bg-neutral-900/80"
       >
-        <div class="bg-white rounded h-min m-auto px-4 py-2 min-w-[25%]">
+        <div class="flex flex-col bg-stone-800 rounded h-min m-auto px-4 py-2 min-w-[25%]">
+          <div class="flex flex-row items-center mb-3">
+            <h1 class="font-mono text-2xl font-black">{{ title }}</h1>
+            <button class="rounded-xl w-fit px-4 py-2 ml-auto font-mono font-bold bg-stone-900 hover:text-orange-400 transition-colors transition-100" @click.prevent.once="emit('close')">X</button>
+          </div>
+
           <slot />
         </div>
       </div>
@@ -15,11 +19,22 @@
 </template>
 
 <script lang="ts" setup>
-  const emit = defineEmits(["closeModal"]);
+  import { onMounted } from "vue";
 
-  withDefaults(defineProps<{
-    isOpen: boolean
-  }>(), { isOpen: false });
+  const emit = defineEmits(["close"]);
+
+  const props = withDefaults(defineProps<{
+    isOpen: boolean,
+    title: string,
+  }>(), { isOpen: false, title: "Title" });
+
+  onMounted(() => {
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "Escape" && props.isOpen) {
+        emit("close");
+      }
+    });
+  });
 </script>
 
 <style scoped>
