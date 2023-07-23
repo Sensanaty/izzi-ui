66e58809cbafe345a33fce17a07236b1069488e4
Luka <salevic@lu-ka.me>
2023-07-23 11:57:24 +0200

    Implement Table Header hiding behavior

--- src/components/admin/table/TableRow.vue
+++ src/components/admin/table/TableRow.vue
@@ -1,26 +1,29 @@
 <template class="flex flex-row">
   <tbody>
     <tr>
-      <td>{{ part.part_number }}</td>
-      <td>{{ part.company_name }}</td>
-      <td>{{ part.added }}</td>
-      <td>{{ part.description }}</td>
-      <td>{{ part.available }}</td>
-      <td>{{ part.reserved }}</td>
-      <td>{{ part.sold }}</td>
-      <td>{{ part.condition }}</td>
-      <td>{{ part.min_cost }}</td>
-      <td>{{ part.min_price }}</td>
-      <td>{{ part.min_order }}</td>
-      <td>{{ part.med_cost }}</td>
-      <td>{{ part.med_price }}</td>
-      <td>{{ part.med_order }}</td>
-      <td>{{ part.max_cost }}</td>
-      <td>{{ part.max_price }}</td>
-      <td>{{ part.max_order }}</td>
-      <td>{{ part.lead_time }}</td>
-      <td>{{ part.quote_type }}</td>
-      <td>{{ part.tag }}</td>
+      <td @click="copy" class="cursor-pointer active:text-green-600 transition-colors">
+        <ph-clipboard-text class="mx-auto" :size="15" weight="fill" />
+      </td>
+      <td v-if="headers.part_number" class="px-1">{{ part.part_number }}</td>
+      <td v-if="headers.company_name">{{ part.company_name }}</td>
+      <td v-if="headers.added" class="text-center">{{ part.added }}</td>
+      <td v-if="headers.description" class="px-1">{{ part.description }}</td>
+      <td v-if="headers.available" class="text-center">{{ part.available }}</td>
+      <td v-if="headers.reserved" class="text-center">{{ part.reserved }}</td>
+      <td v-if="headers.sold" class="text-center">{{ part.sold }}</td>
+      <td v-if="headers.condition" class="text-center">{{ part.condition }}</td>
+      <td v-if="headers.min_cost" class="text-center">{{ part.min_cost }}</td>
+      <td v-if="headers.min_price" class="text-center">{{ part.min_price }}</td>
+      <td v-if="headers.min_order" class="text-center">{{ part.min_order }}</td>
+      <td v-if="headers.med_cost" class="text-center">{{ part.med_cost }}</td>
+      <td v-if="headers.med_price" class="text-center">{{ part.med_price }}</td>
+      <td v-if="headers.med_order" class="text-center">{{ part.med_order }}</td>
+      <td v-if="headers.max_cost" class="text-center">{{ part.max_cost }}</td>
+      <td v-if="headers.max_price" class="text-center">{{ part.max_price }}</td>
+      <td v-if="headers.max_order" class="text-center">{{ part.max_order }}</td>
+      <td v-if="headers.lead_time">{{ part.lead_time }}</td>
+      <td v-if="headers.quote_type">{{ part.quote_type }}</td>
+      <td v-if="headers.tag">{{ part.tag }}</td>
     </tr>
   </tbody>
 </template>
@@ -28,7 +31,37 @@
 <script lang="ts" setup>
   import Part from "~/types/store/part";
 
-  defineProps<{
-    part: Part
+  import { PhClipboardText } from "@phosphor-icons/vue";
+
+  import type { VisibleHeaders } from "~components/admin/types";
+
+  const props = defineProps<{
+    part: Part,
+    headers: VisibleHeaders,
   }>();
+
+  async function copy() {
+    const keys = Object.keys(props.part) as Array<keyof Part>;
+
+    const text = keys.map((part) => {
+      return props.part[part];
+    }).join("\n");
+
+    try {
+      await navigator.clipboard.writeText(text);
+
+      return Promise.resolve(true);
+    } catch (err) {
+      console.error(err);
+
+      // eslint-disable-next-line prefer-promise-reject-errors
+      return Promise.reject(false);
+    }
+  }
 </script>
+
+<style scoped>
+td {
+  @apply break-words text-sm;
+}
+</style>
---
+++ src/components/admin/types.ts
@@ -0,0 +1,22 @@
+export type VisibleHeaders = {
+  sold: boolean;
+  med_cost: boolean;
+  max_cost: boolean;
+  added: boolean;
+  available: boolean;
+  description: boolean;
+  med_order: boolean;
+  quote_type: boolean;
+  condition: boolean;
+  med_price: boolean;
+  max_price: boolean;
+  min_price: boolean;
+  reserved: boolean;
+  min_cost: boolean;
+  company_name: boolean;
+  lead_time: boolean;
+  part_number: boolean;
+  min_order: boolean;
+  tag: boolean;
+  max_order: boolean
+};
