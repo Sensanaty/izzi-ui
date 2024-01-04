a9fcc6588993c49b8fc8e2a3a9699b6515842637
Luka <salevic@lu-ka.me>
2024-01-04 15:20:12 +0100

    Stack the Notifications on top of everything else

--- src/components/notifications/NotificationContainer.vue
+++ src/components/notifications/NotificationContainer.vue
@@ -1,6 +1,6 @@
 <template>
   <Teleport to="body">
-    <div class="font-mono inline-flex column flex-wrap fixed bottom bottom-[20px] right-[15px] max-w-[350px]">
+    <div class="z-10 font-mono inline-flex column flex-wrap fixed bottom bottom-[20px] right-[15px] max-w-[350px]">
       <PopUp
         v-for="(notification, index) in notifications.notifications"
         :key="notification.id"
