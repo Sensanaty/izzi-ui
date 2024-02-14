3bd94deb182e8e97343d9aa4bbffafab4a4e915b
Luka <salevic@lu-ka.me>
2024-02-14 16:21:45 +0800

    Hide 'New Part' link if user isn't logged in

--- src/components/base/AppHeader.vue
+++ src/components/base/AppHeader.vue
@@ -1,47 +1,42 @@
 <template>
-  <header class="flex items-center bg-neutral-800 w-full h-14 px-4 mb-4">
-    <h1 class="font-bold text-center">
-      <RouterLink class="mr-12 text-2xl hover:text-amber-400" :to="auth.loggedIn ? '/admin' : '/'">IZZICUP</RouterLink>
-      <RouterLink class="text-xl hover:text-amber-400" to="/admin/new">New Part</RouterLink>
-    </h1>
+    <header class="flex items-center bg-neutral-800 w-full h-14 px-4 mb-4">
+        <h1 class="font-bold text-center">
+            <RouterLink class="mr-12 text-2xl hover:text-amber-400" :to="auth.loggedIn ? '/admin' : '/'">IZZICUP
+            </RouterLink>
+            <RouterLink v-if="auth.loggedIn" class="text-xl hover:text-amber-400" to="/admin/new">New Part</RouterLink>
+        </h1>
 
-    <div class="flex flex-row items-center ml-auto">
-      <ph-lightbulb-filament v-if="auth.loggedIn" :size="20" class="mr-5 cursor-pointer hover:text-yellow-400" @click="toggleShortcuts" />
+        <div class="flex flex-row items-center ml-auto">
+            <ph-lightbulb-filament v-if="auth.loggedIn" :size="20" class="mr-5 cursor-pointer hover:text-yellow-400"
+                @click="toggleModal" />
 
-      <BaseButton v-if="!auth.loggedIn" class="ml-auto">
-        <RouterLink to="/login">LOGIN</RouterLink>
-      </BaseButton>
+            <BaseButton v-if="!auth.loggedIn" class="ml-auto">
+                <RouterLink to="/login">LOGIN</RouterLink>
+            </BaseButton>
 
-      <BaseButton v-else class="ml-auto" @click="logout">LOGOUT</BaseButton>
-    </div>
-  </header>
+            <BaseButton v-else class="ml-auto" @click="logout">LOGOUT</BaseButton>
+        </div>
 
-  <BaseModal :is-open="isModalOpen" title="Keyboard Shortcuts" @close="toggleShortcuts">
-    <p class="mb-4 font-mono font-bold"><code class="bg-stone-900 p-2 mr-3 rounded text-center">CTRL/⌘ + K</code> Focus on Searchbar</p>
-    <p class="mb-4 font-mono font-bold"><code class="bg-stone-900 p-2 mr-3 rounded text-center">Shift + -&gt;</code> Fetch next page</p>
-    <p class="mb-4 font-mono font-bold"><code class="bg-stone-900 p-2 mr-3 rounded text-center">Shift + &lt;-</code> Fetch previous page</p>
-  </BaseModal>
+        <ShortcutModal :is-open="isModalOpen" @close="toggleModal" />
+    </header>
 </template>
 
 <script setup lang="ts">
-  import { PhLightbulbFilament } from "@phosphor-icons/vue";
+import { PhLightbulbFilament } from "@phosphor-icons/vue";
 
-  import useModal from "~/composables/useModal";
-  import router from "~/modules/router";
-  import useAuthStore from "~/store/auth";
-  import BaseButton from "~components/base/BaseButton.vue";
-  import BaseModal from "~components/base/BaseModal.vue";
+import useModal from "~/composables/useModal";
+import router from "~/modules/router";
+import useAuthStore from "~/store/auth";
+import BaseButton from "~components/base/BaseButton.vue";
+import BaseModal from "~components/base/BaseModal.vue";
+import ShortcutModal from "~components/base/ShortcutModal.vue";
 
-  const auth = useAuthStore();
+const auth = useAuthStore();
 
-  const logout = () => {
+const logout = () => {
     auth.logout();
     router.push("/");
-  };
+};
 
-  const { isModalOpen, toggleModal } = useModal();
-
-  const toggleShortcuts = () => {
-    toggleModal();
-  };
+const { isModalOpen, toggleModal } = useModal();
 </script>
