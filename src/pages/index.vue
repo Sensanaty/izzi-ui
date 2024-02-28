44d6e3594b58d17c26a7a820a0ae4fa167a04a06
Ana Calija <ana.salevic@gmail.com>
2024-02-28 11:56:10 +0100

    changed favicon to DAP instead izzi

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,34 +1,32 @@
 <template>
-  <h1 class="mx-auto font-bold text-4xl text-center mb-5">
-    PT DHARMA ANGKASA PUTRA was established in February 13 2024
+  <h1 class="mx-1000 font-bold text-4xl text-center mb-5">PT Dharma Angkasa Putra
   </h1>
 
   <div class="mx-auto flex justify-between w-1/2 mb-3">
     <AddressBox title="Main Office" address="Jl. Tukad Buana Gunung Sari, Gg. Baja No.1 Batukandik"
-      sub-address="80117, Denpasar, Bali" />
+      sub-address="80117, Denpasar, Bali, Indonesia" />
   </div>
 
   <div class="flex flex-col justify-center items-center mx-auto w-1/2 bg-neutral-800 p-4 rounded">
     <h1 class="text-3xl font-bold text-center">Contact Us</h1>
-
     <a href="mailto:dharmaangkasaputra@gmail.com"
       class="font-bold text-xl underline text-center hover:decoration-blue-300">dharmaangkasaputra@gmail.com</a>
 
     <form class="w-full mt-4">
       <div class="flex flex-row justify-between">
         <div class="flex flex-col min-w-[45%]">
-          <label for="name">name</label>
+          <label for="name">Name</label>
           <input id="name" v-model="name" type="text">
         </div>
 
         <div class="flex flex-col min-w-[45%]">
-          <label for="email">email</label>
+          <label for="email">Email</label>
           <input id="email" v-model="email" type="email">
         </div>
       </div>
 
       <div class="flex flex-col mt-3">
-        <label for="message">message</label>
+        <label for="message">Message</label>
         <textarea id="message" v-model="message" name="message" cols="40" rows="10"
           class="rounded text-black p-2 mt-1 resize-none" />
       </div>
