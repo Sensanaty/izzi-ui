63a072c4fae7170df687dad3094a032010271935
Luka <salevic@lu-ka.me>
2024-03-12 09:36:32 +0100

    Run eslint

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container mx-auto px-4">
     <div class="px-4 py-9 flex items-center justify-center gap-8">
-    <img width="256" src="/assets/images/logo_full.svg" alt="DAP logo">
-    <h1 class="pt-2 text-3xl text-yellow-500">WELCOME TO PT DHARMA ANGKASA PUTRA</h1>
+      <img width="256" src="/assets/images/logo_full.svg" alt="DAP logo">
+      <h1 class="pt-2 text-3xl text-yellow-500">WELCOME TO PT DHARMA ANGKASA PUTRA</h1>
     </div>
-     <h2 class="text-2xl font-medium text-yellow-500 py-8">WHAT WE DO</h2>
+    <h2 class="text-2xl font-medium text-yellow-500 py-8">WHAT WE DO</h2>
 
     <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-4 m-auto py-6">
       <div>
