c8ea96de5c5ce94327531ffee74406c56844679f
Luka <salevic@lu-ka.me>
2024-10-24 14:44:46 +0200

    Add a default border style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      "borderColor": { "DEFAULT": "rgb(64 64 64 / 0.6)" },
+    },
   },
   plugins: [],
 } satisfies Config;
