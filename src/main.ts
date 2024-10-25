f5298f4f1535706a9bda1df06bbdf66b1b141f77
Luka <salevic@lu-ka.me>
2024-10-25 20:40:43 +0200

    Add Base Input & Button as global components

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,15 @@ import "@/assets/main.css";
 import App from "@/App.vue";
 import router from "@/router";
 
+import BaseButton from "@/components/Base/BaseButton.vue";
+import BaseInput from "@/components/Base/Form/BaseInput.vue";
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+app.component("BaseButton", BaseButton);
+app.component("BaseInput", BaseInput);
 
 app.mount("#app");
