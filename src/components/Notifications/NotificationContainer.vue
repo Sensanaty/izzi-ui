5e7f89f003f03e79c72591767238fd9ea6134c42
Luka <salevic@lu-ka.me>
2024-10-26 15:36:35 +0200

    Create a nice transition for the notifications

--- src/components/Notifications/NotificationContainer.vue
+++ src/components/Notifications/NotificationContainer.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="flex flex-col fixed bottom-5 left-4 max-w-96 z-10">
-    <NotificationBox v-for="notification in notifications" :key="notification.id" :notification="notification" />
+    <TransitionGroup name="notification-slide">
+      <NotificationBox v-for="notification in notifications" :key="notification.id" :notification="notification" />
+    </TransitionGroup>
   </div>
 </template>
 
@@ -11,3 +13,24 @@ import { storeToRefs } from "pinia";
 
 const { notifications } = storeToRefs(useNotificationStore());
 </script>
+
+<style scoped lang="postcss">
+.notification-slide-enter-active,
+.notification-slide-leave-active {
+  transition: all 100ms ease-in-out;
+}
+
+.notification-slide-enter-from {
+  opacity: 0;
+  transform: translateX(-500px);
+}
+
+.notification-slide-leave-to {
+  opacity: 0;
+  transform: translateX(-500px);
+}
+
+.notification-slide-move {
+  transition: transform 100ms ease-in-out;
+}
+</style>
