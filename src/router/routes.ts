c84434e135ea2265c1c3dd955bb7b5115450e8a7
Luka <salevic@lu-ka.me>
2024-12-07 15:45:05 +0100

    Add empty Part routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,6 +5,10 @@ import type { RouteRecordRaw } from "vue-router";
 export const ROUTE = {
   HOME: "home",
   LOGIN: "login",
+
+  PART_INDEX: "partIndex",
+  PART_CREATE: "partCreate",
+  PART_EDIT: "partEdit",
 } as const;
 
 // Ensures that `ROUTE_PATH` can only contain keys that belong in `ROUTE`
@@ -13,6 +17,11 @@ type ValidPathKey = { [k in keyof typeof ROUTE]: string };
 export const ROUTE_PATH: ValidPathKey = {
   HOME: "/",
   LOGIN: "/login",
+
+  // Part-related Routes
+  PART_INDEX: "/part",
+  PART_CREATE: "/part",
+  PART_EDIT: "/part/:id",
 } as const;
 
 const publicRoutes: RouteRecordRaw[] = [
@@ -30,6 +39,27 @@ const privateRoutes: RouteRecordRaw[] = [
     name: ROUTE.HOME,
     component: HomeView,
     meta: { auth: true, title: "Parts" },
+  },
+
+  {
+    path: ROUTE_PATH.PART_INDEX,
+    name: ROUTE.PART_INDEX,
+    component: import("@/views/Part/PartIndex.vue"),
+    meta: { auth: true },
+    children: [
+      {
+        path: ROUTE_PATH.PART_CREATE,
+        name: ROUTE.PART_CREATE,
+        component: import("@/views/PartEdit.vue"),
+        meta: { title: "Parts - Create", isCreatePage: true },
+      },
+      {
+        path: ROUTE_PATH.PART_EDIT,
+        name: ROUTE.PART_EDIT,
+        component: import("@/views/PartEdit.vue"),
+        meta: { title: "Parts - Edit", isCreatePage: false },
+      },
+    ],
   },
 ];
 
--- src/views/layouts/MainLayout.vue
+++ src/views/layouts/MainLayout.vue
@@ -1,26 +1,42 @@
 <template>
   <header class="flex w-full items-center border-b border-r bg-neutral-900 px-4 py-1">
     <nav class="flex w-full items-center">
-      <RouterLink
-        v-for="route in navlinks"
-        :key="route.route"
-        :to="route.route"
-        class="mr-4 flex items-center text-lg font-bold underline decoration-transparent decoration-2 last:mr-0 hover:!decoration-emerald-500"
-        exact-active-class="!decoration-emerald-700 text-emerald-600"
-      >
-        <component
-          :is="route.icon"
-          class="mr-1.5 transition-colors duration-75"
-          v-bind="{ weight: 'duotone', color: 'currentColor' }"
-        />
+      <template v-for="route in navlinks" :key="route.route">
+        <div class="mr-4 flex items-center">
+          <RouterLink
+            :to="route.route"
+            class="flex items-center text-lg font-bold underline decoration-transparent decoration-2 hover:!decoration-emerald-500"
+            exact-active-class="!decoration-emerald-700 text-emerald-600"
+          >
+            <component
+              :is="route.icon"
+              v-if="route.icon"
+              class="mr-1.5 transition-colors duration-75"
+              v-bind="{ weight: 'duotone', color: 'currentColor' }"
+            />
+            <span class="text-white">{{ route.text }}</span>
+          </RouterLink>
 
-        <span class="text-white">{{ route.text }}</span>
-      </RouterLink>
+          <template v-if="route.subroutes && isRouteActive(route.route)">
+            <span class="mx-2 select-none text-neutral-500">|</span>
+
+            <RouterLink
+              v-for="subRoute in route.subroutes"
+              :key="subRoute.route"
+              :to="subRoute.route"
+              class="mr-2 text-lg font-semibold underline decoration-transparent decoration-2 last:mr-0 hover:!decoration-emerald-500"
+              exact-active-class="!decoration-emerald-700 text-emerald-600"
+            >
+              <span class="text-white">{{ subRoute.text }}</span>
+            </RouterLink>
+          </template>
+        </div>
+      </template>
 
       <RouterLink
         v-if="!authStore.loggedIn"
         :to="ROUTE_PATH.LOGIN"
-        class="ml-auto flex items-center text-lg font-bold underline decoration-transparent decoration-2 last:mr-0 hover:!decoration-emerald-500"
+        class="ml-auto flex items-center text-lg font-bold underline decoration-transparent decoration-2 hover:!decoration-emerald-500"
         exact-active-class="!decoration-emerald-700"
       >
         Login
@@ -29,7 +45,7 @@
       <button
         v-else
         type="button"
-        class="ml-auto flex items-center text-lg font-bold underline decoration-transparent decoration-2 last:mr-0 hover:!decoration-emerald-500"
+        class="ml-auto flex items-center text-lg font-bold underline decoration-transparent decoration-2 hover:!decoration-emerald-500"
         @click.prevent="authStore.logout"
       >
         Logout
@@ -45,18 +61,34 @@
 <script setup lang="ts">
 import { ROUTE_PATH } from "@/router/routes";
 import { PhAirplaneTilt } from "@phosphor-icons/vue";
+import { useRoute } from "vue-router";
 
 import type { Component } from "vue";
 import useAuthStore from "@/stores/auth";
+
+type SubNavlink = Omit<AdminNavlinks, "icon" | "subroutes"> & { isSubLink: true };
 
 type AdminNavlinks = {
   text: string;
   route: string;
   classes?: string;
-  icon: Component;
+  icon?: Component;
+  subroutes?: SubNavlink[];
 };
 
-const navlinks: AdminNavlinks[] = [{ text: "Parts", route: ROUTE_PATH.HOME, icon: PhAirplaneTilt }];
+const navlinks: AdminNavlinks[] = [
+  {
+    text: "Parts",
+    route: ROUTE_PATH.HOME,
+    icon: PhAirplaneTilt,
+    subroutes: [{ text: "Create", route: ROUTE_PATH.PART_CREATE, isSubLink: true }],
+  },
+];
 
+const route = useRoute();
 const authStore = useAuthStore();
+
+const isRouteActive = (path: string) => {
+  return route.path.startsWith(path);
+};
 </script>
