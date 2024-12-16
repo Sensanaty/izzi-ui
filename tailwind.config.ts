1549e02d9bab9f9568e9d2b68daae55a2c4c77ba
Luka <salevic@lu-ka.me>
2024-12-16 16:46:24 +0100

    Add `screens` to the tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
+    screens: {
+      "2xl": { "max": "1535px" },
+      "xl": { "max": "1279px" },
+      "lg": { "max": "1023px" },
+      "md": { "max": "767px" },
+      "sm": { "max": "639px" },
+    },
     extend: {
       "borderColor": { "DEFAULT": "rgb(64 64 64 / 0.6)" },
     },
