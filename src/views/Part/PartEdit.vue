7261a1e504e43711baed3640d6cffb048af225ea
Luka <salevic@lu-ka.me>
2024-12-20 16:40:58 +0100

    Create the Part Edit page

--- src/views/Part/PartEdit.vue
+++ src/views/Part/PartEdit.vue
@@ -1,3 +1,29 @@
 <template>
-  Edit part
+  <h1 v-if="isCreatePage" class="mb-2.5 text-2xl font-bold">
+    Creating new Part
+  </h1>
+
+  <h1 v-else class="mb-2.5 text-2xl font-bold">
+    Editing Part <span class="font-mono">{{ route.params.id }}</span>
+  </h1>
+
+  <PartForm :is-create-page="isCreatePage" />
 </template>
+
+<script setup lang="ts">
+import { computed, onBeforeMount } from "vue";
+import { useRoute } from "vue-router";
+import PartForm from "@/components/PartForm.vue";
+import usePartsStore from "@/stores/parts.ts";
+
+const route = useRoute();
+
+const partStore = usePartsStore();
+const isCreatePage = computed(() => !!route.meta?.isCreatePage);
+
+onBeforeMount(async () => {
+  if (!isCreatePage.value && route.params.id) {
+    await partStore.getPart(Number(route.params.id));
+  }
+});
+</script>
