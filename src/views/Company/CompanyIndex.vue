3834930b96ed282ece27db1edf94692e364d4bfe
Luka <salevic@lu-ka.me>
2024-12-28 16:26:54 +0100

    Add fuzzysort to project

--- src/views/Company/CompanyIndex.vue
+++ src/views/Company/CompanyIndex.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>hello</h1>
+  <div class="grid auto-rows-auto grid-cols-3 gap-x-2 gap-y-3.5 md:grid-cols-2">
+    <CompanyCard
+      v-for="company in companyStore.companies"
+      :key="company.id"
+      :company="company"
+    />
+  </div>
 </template>
+
+<script setup lang="ts">
+import { onBeforeMount } from "vue";
+import useCompanyStore from "@/stores/company.ts";
+import CompanyCard from "@/components/Company/CompanyCard.vue";
+
+const companyStore = useCompanyStore();
+
+onBeforeMount(async () => {
+  await companyStore.getAllCompanies();
+});
+</script>
