bb025751b060bc720cd3fb19c92cb768887a592a
Sensanaty <salevic@lu-ka.me>
2026-09-02 21:12:11 +0200

    Format `vite` config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
-import { defineConfig } from "vitest/config";
 import vueDevTools from "vite-plugin-vue-devtools";
+import { defineConfig } from "vitest/config";
 
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig(({ command }) => ({
-  plugins: [
-    tailwindcss(),
-    vue(),
-    ...(command === "serve" ? [vueDevTools()] : []),
-  ],
+  plugins: [tailwindcss(), vue(), ...(command === "serve" ? [vueDevTools()] : [])],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
